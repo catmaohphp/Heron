5fd0d5eae023a6fa9c1f2a29222f005dc9e277d4
bijun <bijun@mail.ustc.edu.cn>
2023-01-22 12:39:18 +0800

    key components

---
+++ env.sh
@@ -0,0 +1,7 @@
+export TVM_HOME=/lustre/S/bijun/work/tvm
+export VTA_HW_PATH=$TVM_HOME/3rdparty/vta-hw
+export SS_HOME=$(pwd)/../
+export PYTHONPATH=$SS_HOME:$PYTHONPATH
+export PYTHONPATH=$TVM_HOME/vta/python:$PYTHONPATH
+export OMP_NUM_THREADS=32
+export OPENBLAS_NUM_THREADS=1
